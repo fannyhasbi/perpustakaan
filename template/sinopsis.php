adc719a3c53905a010c360f797439d76546bc73c
Fanny Hasbi <fnyhsbi@gmail.com>
2016-12-05 15:31:52 +0700

    Penggantian format file

---
+++ template/sinopsis.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sinopsis {judul}</title>
+	<link rel="stylesheet" href="view/main.css"/>
+</head>
+<body>
+
+<h1>{judul}</h1>
+<h4>Ditulis oleh {pengarang}</h4>
+<h4>Diterbitkan oleh {penerbit}</h4>
+<p>Jenis: {jenis}</p><br>
+<p>{konten}</p>
+
+</body>
+</html>
