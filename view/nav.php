be4477a0888c1775c14685482ecac5241a8b5053
Fanny Hasbi <fnyhsbi@gmail.com>
2016-12-06 22:10:29 +0700

    Update tampilan navigasi

--- view/nav.php
+++ view/nav.php
@@ -12,11 +12,30 @@ if(isset($_SESSION['login'])){
 			</button>
 		</div>
 		<div class="collapse navbar-collapse" id="navigasi">
+			<?php
+			if($_SERVER['PHP_SELF'] == "/perpustakaan/index.php"){
+			?>
 			<ul class="nav navbar-nav navbar-right">
 				<li><a href="buku.php">BUKU</a></li>
 				<li><a href='pinjam.php'>PINJAM</a></li>
-				<li><a href='logout.php'>LOGOUT</a></li>
 			</ul>
+			<?php
+			}
+			if($_SERVER['PHP_SELF'] != "/perpustakaan/index.php"){
+			?>
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown"><a href="#" data-toggle="dropdown"><?php echo $_SESSION['n_depan'];?> <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="pinjam.php">Pinjam</a></li>
+						<li><a href="buku.php">Buku</a></li>
+						<li class="divider"></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+			<?php
+			}
+			?>
 		</div>
 	</div>
 </nav>
