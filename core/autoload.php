2b856500823a0201e431e0b68edf222c2f277971
fannyhasbi <fnyhsbi@gmail.com>
2018-10-29 21:14:59 +0700

    Add core functions and autoloader

---
+++ core/autoload.php
@@ -0,0 +1,6 @@
+<?php
+// autoloader
+// untuk meload beberapa fungsi utama yang dibutuhkan
+// agar require file tidak terlalu banyak di setiap file
+
+require_once __DIR__."/../function/db.php";
---
+++ function/db.php
@@ -0,0 +1,9 @@
+<?php
+// Kredensial database yang diperlukan
+
+$host     = "localhost";
+$uname    = "root";
+$pass     = "";
+$database = "perpustakaan";
+
+$connect = mysqli_connect($host, $uname, $pass, $database) OR die("Database connection error");
