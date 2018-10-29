3a384aa3fc11351f992502be94cbe97e61dd57f5
fannyhasbi <fnyhsbi@gmail.com>
2018-10-29 22:36:14 +0700

    Login functionality

--- member/login.php
+++ member/login.php
@@ -1,3 +1,41 @@
+<?php
+session_start();
+
+// jika sudah login
+if(isset($_SESSION['login-member']))
+  header('Location: ../buku/index.php');
+
+// jika klik tombol login
+if(isset($_POST['login-member']) && isset($_POST['username']) && isset($_POST['password'])){
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  require_once __DIR__."/../core/autoload.php";
+  
+  $query = "SELECT * FROM anggota WHERE username = '". $username ."'";
+  $result= mysqli_query($connect, $query);
+
+  // Cek apakah username terdaftar
+  if($result->num_rows == 0)
+    die("Username atau password salah");
+
+  $row = mysqli_fetch_assoc($result);
+
+  // cek apakah password input sama dengan password di database
+  // karena menggunakan enkripsi, maka perlu menggunakan fungsi password_verify
+  // untuk memvalidasi
+  if(!password_verify($password, $row['password']))
+    die("Username atau password salah");
+
+  $_SESSION['username'] = $row['username'];
+  $_SESSION['nama']     = $row['nama'];
+  $_SESSION['status']   = $row['status'];
+  $_SESSION['alamat']   = $row['alamat'];
+
+  header("Location: ../buku/index.php");
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,10 +47,10 @@
 <body>
   <h1>Login Member</h1>
 
-  <form action="">
+  <form action="" method="post">
     <div>
       <label for="username">Username</label><br />
-      <input type="text" name="username" placeholder="username" />
+      <input type="text" name="username" placeholder="username" autofocus />
 
       <br><br>
 
